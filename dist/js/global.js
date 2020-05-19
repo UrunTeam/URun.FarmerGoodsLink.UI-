a2a71c86161b3cd65d81f77c3d8b561941c99665
luoxuanming <1316570222@qq.com>
2020-05-19 18:35:00 +0800

    添加页面

--- dist/js/global.js
+++ dist/js/global.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
     //模块js初始化
     commonJs.fn.init();
-
+    //分页
+    $(".paging").Paging({
+      dom: {
+        pageno: true, // 页码：1、2、3、4、5
+        pagesize: false, // 每页显示多少条
+        datacount: true, // 数据总数
+        pagecount: true, // 页码总数
+        pagebtn: true, // 上一页，下一页按钮
+        pagefirst: false, // 首页按钮
+        pagelast: false, // 尾页按钮
+        fastgo: true // 快速跳转
+      },
+      pagesize: 5,
+      current: 1,
+      count: 100,
+      pageSizeList: [10, 20, 30, 40, 50],
+      prevTpl: "<",
+      nextTpl: ">"
+    });
 });
 
 var commonJs = $(window).commonJs || {};
@@ -9,9 +27,9 @@ var commonJs = $(window).commonJs || {};
 commonJs.fn = {
     init : function(){
         var _this = this;
-        _this.dropdown();
+        // _this.dropdown();
         _this.icheck();
-        _this.nicescroll();
+        // _this.nicescroll();
 
     },
 
@@ -34,11 +52,24 @@ commonJs.fn = {
 
     // 复选框
     icheck : function () {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_flat-blue',
-            radioClass: 'iradio_flat-blue',
-            increaseArea: '20%'
-        });
+        // $('input').iCheck({
+        //     checkboxClass: 'icheckbox_flat-blue',
+        //     radioClass: 'iradio_flat-blue',
+        //     increaseArea: '20%'
+        // });
+
+        $('body').on("click",'.check-radio',function() {
+          //复选
+          // if($(this).hasClass("check")) {
+          //   $(this).removeClass("check");
+          // } else {
+          //   $(this).addClass("check");
+          // }
+
+          //单选
+          $(this).addClass("check");
+          $(this).parents().siblings().find('.check-radio').removeClass("check");
+        })
     }
 
 };
--- gulpfile.js
+++ gulpfile.js
@@ -23,101 +23,101 @@ var tplDir = './app/templates';  // 模版目录
 
 // ----------------静态页面开发流程----------------begin
 //默认任务
-gulp.task('default',['server']);
+gulp.task('default', ['server']);
 
 //静态服务器
-gulp.task('server', function() {
-    runSequence('build', 'watch', function(){
-        // .init 启动服务器
-        browserSync.init({
-            server: {
-                baseDir: "./dist"
-            },
-            port: 8765,
-            startPath: "./html/index.html",
-            open: "external"
-        },function(){
-            gulp.watch(['dist/**','!dist/css/*.map','!dist/css/*.min.css']).on('change', reload);
-        });
+gulp.task('server', function () {
+  runSequence('build', 'watch', function () {
+    // .init 启动服务器
+    browserSync.init({
+      server: {
+        baseDir: "./dist"
+      },
+      port: 8765,
+      startPath: "./html/index.html",
+      open: "external"
+    }, function () {
+      gulp.watch(['dist/**', '!dist/css/*.map', '!dist/css/*.min.css']).on('change', reload);
     });
+  });
 });
 
 // 监视源文件变化,自动编译less,自动复制html和js
 gulp.task('watch', () => {
-    gulp.watch(['./app/html/*.html','./app/html/*.json','./app/templates/**'], ['buildhtml']);
-    gulp.watch('./app/less/**', ['buildcss']);
-    gulp.watch('./app/js/*.js', ['checkjs']);
-    gulp.watch('./app/images/**', ['images']);
+  gulp.watch(['./app/html/*.html', './app/html/*.json', './app/templates/**'], ['buildhtml']);
+  gulp.watch('./app/less/**', ['buildcss']);
+  gulp.watch('./app/js/*.js', ['checkjs']);
+  gulp.watch('./app/images/**', ['images']);
 });
 
 //整体构建
 gulp.task('build', ['buildcss', 'checkjs', 'buildhtml', 'images', 'copy']);
 
 //编译html文件到dist
-gulp.task('buildhtml', function() {
+gulp.task('buildhtml', function () {
   gulp.src('./app/html/*.html')
-    .pipe(c$.changed('./app/html/**', {extension:'.html'}))
+    .pipe(c$.changed('./app/html/**', { extension: '.html' }))
     .pipe(c$.data(function (file) {
-        var filePath = file.path;
-        var file_json = path.join(path.dirname(filePath), path.basename(filePath, '.html') + '.json');
-        //console.log(1, JSON.parse(fs.readFileSync(tplDir + '/global.json')));
-        if(fs.existsSync(file_json)){
-            //合并全局json 和 自身的json
-            return Object.assign(JSON.parse(fs.readFileSync(tplDir + '/global.json')), {
-                local: JSON.parse(fs.readFileSync( file_json ))
-            });
-        }
-        return JSON.parse(fs.readFileSync(tplDir + '/global.json'));
+      var filePath = file.path;
+      var file_json = path.join(path.dirname(filePath), path.basename(filePath, '.html') + '.json');
+      //console.log(1, JSON.parse(fs.readFileSync(tplDir + '/global.json')));
+      if (fs.existsSync(file_json)) {
+        //合并全局json 和 自身的json
+        return Object.assign(JSON.parse(fs.readFileSync(tplDir + '/global.json')), {
+          local: JSON.parse(fs.readFileSync(file_json))
+        });
+      }
+      return JSON.parse(fs.readFileSync(tplDir + '/global.json'));
     }))
-    .pipe(c$.ejs().on('error', function(err) {
-        console.log("Ejs Error!", err.message);
-            this.end();
+    .pipe(c$.ejs().on('error', function (err) {
+      console.log("Ejs Error!", err.message);
+      this.end();
     }))
     .pipe(gulp.dest('./dist/html'));
 });
 
 //js语法检查
-gulp.task('checkjs', function() {
-    gulp.src('./app/js/*.js')
-        .pipe(c$.changed('./dist/js', {extension:'.js'}))//仅监听有变化的js
-        .pipe(c$.jshint())
-        .pipe(c$.jshint.reporter('default'))
-        .pipe(gulp.dest('./dist/js'));
+gulp.task('checkjs', function () {
+  gulp.src('./app/js/*.js')
+    .pipe(c$.changed('./dist/js', { extension: '.js' }))//仅监听有变化的js
+    .pipe(c$.jshint())
+    .pipe(c$.jshint.reporter('default'))
+    .pipe(gulp.dest('./dist/js'));
 });
 
 //构建css,串行执行less和cleancss
-gulp.task('buildcss', function(callback) {
-    runSequence('less', 'cleancss', callback);
+gulp.task('buildcss', function (callback) {
+  runSequence('less', 'cleancss', callback);
 });
 
 //less任务
 gulp.task('less', function () {
-    return gulp.src('./app/less/*.less') //该任务针对的文件
-        .pipe(c$.sourcemaps.init())
-        .pipe(c$.less()) //该任务调用的模块
-        .on('error', function(err) {
-            console.log("Less Error!", err.message);
-            this.end();
-        })
-        .pipe(c$.autoprefixer({
-            browsers: ['last 2 versions', 'Android >= 4.0'],
-            cascade: true, //是否美化属性值 默认：true
-            remove: false //是否去掉不必要的前缀 默认：true
-        }))
-        .pipe(c$.sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/css')); //将会在dist/css下生成main.css
+  return gulp.src('./app/less/*.less') //该任务针对的文件
+    .pipe(c$.sourcemaps.init())
+    .pipe(c$.less()) //该任务调用的模块
+    .on('error', function (err) {
+      console.log("Less Error!", err.message);
+      this.end();
+    })
+    .pipe(c$.autoprefixer({
+      browsers: ['last 2 versions', 'Android >= 4.0'],
+      cascade: true, //是否美化属性值 默认：true
+      remove: false //是否去掉不必要的前缀 默认：true
+    }))
+    .pipe(c$.sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/css')); //将会在dist/css下生成main.css
 });
 
 //压缩css
-gulp.task('cleancss', function() {
-    return gulp.src(['./dist/css/*.css','!./dist/css/*.min.css'])//只选出dist/css中不含有.min.css的文件
-        .pipe(c$.cleanCss())
-        .pipe(c$.rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist/css'));
+gulp.task('cleancss', function () {
+  return gulp.src(['./dist/css/*.css', '!./dist/css/*.min.css'])//只选出dist/css中不含有.min.css的文件
+    .pipe(c$.cleanCss())
+    .pipe(c$.rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/css'));
 });
 
 // 图片优化
-gulp.task('images', function(){
+gulp.task('images', function () {
   return gulp.src('./app/images/**')
     // .pipe(c$.imagemin({
     //   progressive: true,
@@ -127,57 +127,60 @@ gulp.task('images', function(){
 });
 
 // 拷贝前端资源
-gulp.task('copy', function(){
-    gulp.src([
-      './app/libs/bootstrap/dist/js/bootstrap.min.js',
-      '!./app/libs/bootstrap/dist/js/npm.js'
-    ])
+gulp.task('copy', function () {
+  gulp.src([
+    './app/libs/bootstrap/dist/js/bootstrap.min.js',
+    '!./app/libs/bootstrap/dist/js/npm.js'
+  ])
     .pipe(gulp.dest('./dist/libs/bootstrap/dist'));
 
-    gulp.src([
-      './app/libs/jquery/jquery.js',
-      './app/libs/jquery/jquery.min.js',
-      './app/libs/jquery/jquery.min.map'
-    ])
+  gulp.src([
+    './app/libs/jquery/jquery.js',
+    './app/libs/jquery/jquery.min.js',
+    './app/libs/jquery/jquery.min.map'
+  ])
     .pipe(gulp.dest('./dist/libs/jquery'));
 
-    // gulp.src([
-    //   './app/libs/echarts/**'
-    // ])
-    // .pipe(gulp.dest('./dist/libs/echarts'));
+  // gulp.src([
+  //   './app/libs/echarts/**'
+  // ])
+  // .pipe(gulp.dest('./dist/libs/echarts'));
 
-    // gulp.src([
-    //   './app/libs/countUp/dist/countUp.min.js'
-    // ])
-    // .pipe(gulp.dest('./dist/libs/countUp/dist'));
+  // gulp.src([
+  //   './app/libs/countUp/dist/countUp.min.js'
+  // ])
+  // .pipe(gulp.dest('./dist/libs/countUp/dist'));
 
-    gulp.src([
-      './app/libs/nicescroll/**'
-    ])
-    .pipe(gulp.dest('./dist/libs/nicescroll'));
+  // gulp.src([
+  //   './app/libs/nicescroll/**'
+  // ])
+  // .pipe(gulp.dest('./dist/libs/nicescroll'));
 
-    gulp.src([
+  // gulp.src([
 
-      './app/libs/icheck/icheck.min.js'
-    ])
-    .pipe(gulp.dest('./dist/libs/icheck'));
+  //   './app/libs/icheck/icheck.min.js'
+  // ])
+  // .pipe(gulp.dest('./dist/libs/icheck'));
 
-    gulp.src([
-      './app/libs/icheck/skins/**'
-    ])
-    .pipe(gulp.dest('./dist/libs/icheck/skins'));
+  // gulp.src([
+  //   './app/libs/icheck/skins/**'
+  // ])
+  // .pipe(gulp.dest('./dist/libs/icheck/skins'));
 
-    //  gulp.src([
-    //   './app/libs/datetimepicker/**'
-    // ])
-    // .pipe(gulp.dest('./dist/libs/datetimepicker'));
-    
-    // gulp.src([
-    //   './app/libs/jquery.paging/**'
-    // ])
-    // .pipe(gulp.dest('./dist/libs/jquery.paging'));
+   gulp.src([
+    './app/libs/datetimepicker/**'
+  ])
+  .pipe(gulp.dest('./dist/libs/datetimepicker'));
 
+  // gulp.src([
+  //   './app/libs/jquery.paging/**'
+  // ])
+  // .pipe(gulp.dest('./dist/libs/jquery.paging'));
 
+  gulp.src([
+    './app/libs/jquery.paging/**'
+  ])
+    .pipe(gulp.dest('./dist/libs/jquery.paging'));
 
 });
 // ----------------静态页面开发流程----------------end
